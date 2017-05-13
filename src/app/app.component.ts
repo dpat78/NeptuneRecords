5593442f25075b9e9365f01c0a22d04dcd5a9489
RedCometVictory <acemiranda13@att.net>
2017-05-12 20:12:57 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from './app.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   providers: [AppService]
+  
 })
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,28 @@ import { BooksComponent } from './books/books.component';
 import { BookService } from './books/book.service';
 import { BookCreateComponent } from './books/book-create/book-create.component';
 import { BookComponent } from './books/book/book.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { OrderComponent } from './order/order.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { UserComponent } from './user/user.component';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
+import { RegisterComponent } from './register/register.component';
+import { SidebarRightComponent } from './sidebar-right/sidebar-right.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
 
 // Routes
 const appRoutes: Routes = [
-  { path: '', component: BooksComponent },
+  { path: '', component: HomeComponent },
   { path: 'book/:id',   component: BookComponent },
-  { path: 'create',   component: BookCreateComponent }
+  { path: 'create',   component: BookCreateComponent },
+  { path: 'about',   component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'gallery', component: GalleryComponent }
+
 ];
 
 @NgModule({
@@ -24,7 +40,19 @@ const appRoutes: Routes = [
     AppComponent,
     BooksComponent,
     BookCreateComponent,
-    BookComponent
+    BookComponent,
+    NavComponentComponent,
+    MainFooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    OrderComponent,
+    GalleryComponent,
+    UserComponent,
+    EmployeeLoginComponent,
+    RegisterComponent,
+    SidebarRightComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
